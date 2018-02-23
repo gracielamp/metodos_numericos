410d46dc0e674ef3db824a15746626ec8ff6b664
Graciela <36769513+gracielamp@users.noreply.github.com>
2018-02-23 12:21:49 -0500

    Add files via upload

---
+++ P_Metodos_Grafico.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p_metodos_grafico;
+
+/**
+ *
+ * @author graciela
+ */
+public class P_Metodos_Grafico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new Menu().setVisible(true);
+    }
+    
+}
